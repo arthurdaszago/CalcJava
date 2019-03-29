a00b94f5ca94ecc22f9d74d07715801ac0d2e64d
arthurdaszago <arthur.zagoaz@hotmail.com>
2019-03-29 20:59:10 -0300

    Meu primeiro envio

---
+++ Calculadora/src/Calc/Metodos.java
@@ -0,0 +1,21 @@
+package Calc;
+
+public class Metodos {
+	private double num1;
+	private double num2;
+	public double getNum1() {
+		return num1;
+	}
+	public void setNum1(double num1) {
+		this.num1 = num1;
+	}
+	public double getNum2() {
+		return num2;
+	}
+	public void setNum2(double num2) {
+		this.num2 = num2;
+	}
+	public double soma() {
+		return (this.getNum1()+this.getNum2());
+	}
+}
