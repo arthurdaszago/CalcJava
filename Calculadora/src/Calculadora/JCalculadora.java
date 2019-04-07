dd8c001c8bf49b0ab64794029ade2528076f6f0d
arthurdaszago <arthur.zagoaz@hotmail.com>
2019-04-07 15:16:38 -0300

    Adc de Tfs para recebimento de numeros

--- Calculadora/src/Calculadora/JCalculadora.java
+++ Calculadora/src/Calculadora/JCalculadora.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
@@ -12,6 +13,8 @@ import java.awt.Font;
 public class JCalculadora extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField tf1;
+	private JTextField tf2;
 
 	/**
 	 * Launch the application.
@@ -44,5 +47,23 @@ public class JCalculadora extends JFrame {
 		lblCalculadora.setFont(new Font("Tahoma", Font.BOLD, 18));
 		lblCalculadora.setBounds(165, 0, 202, 33);
 		contentPane.add(lblCalculadora);
+		
+		JLabel lblNmero = new JLabel("N\u00FAmero 1");
+		lblNmero.setBounds(10, 46, 46, 14);
+		contentPane.add(lblNmero);
+		
+		JLabel lblNmero_1 = new JLabel("N\u00FAmero 2");
+		lblNmero_1.setBounds(10, 88, 46, 14);
+		contentPane.add(lblNmero_1);
+		
+		tf1 = new JTextField();
+		tf1.setBounds(86, 44, 86, 20);
+		contentPane.add(tf1);
+		tf1.setColumns(10);
+		
+		tf2 = new JTextField();
+		tf2.setBounds(86, 85, 86, 20);
+		contentPane.add(tf2);
+		tf2.setColumns(10);
 	}
 }
